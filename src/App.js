683a9e81bb454ecd50bd896cbf8043eb17216c59
Emmanuel Estrada <e.estrada1917@gmail.com>
2022-04-29 12:46:24 -0500

    Componentes sin usar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Pokeinfo from './components/Pokeinfo';
-import PokedexGeneracion from './components/PokedexGeneracion';
 import './css/main.css';
 
 class App extends React.Component{
