574cf28b2dff930cd372374108c4ed8727e3ca84
Emmanuel Estrada <e.estrada1917@gmail.com>
2022-04-29 11:33:53 -0500

    Menu de hamburguesa

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,13 +7,19 @@ const Navbar = () => {
         <span className='logo'>
             <img src='../img/pokeball.png' className='pokeball'/>
         </span>
-        <nav>
-          <ul>
-            <li><a href='#'>Inicio</a></li>
-            <li><a href='#'>Pokemon</a></li>
-            <li><a href='#'>Contacto</a></li>
-          </ul>
-        </nav>
+        <div className='hamburger-menu'>
+          <nav>
+            <input id="toggle" type="checkbox" />
+            <label class="botonBurguer" for="toggle">
+              <span></span>
+            </label>
+            <ul className='menuLateral'>
+              <li><a href='#' className='item'>Inicio</a></li>
+              <li><a href='#' className='item'>Pokemon</a></li>
+              <li><a href='#' className='item'>Contacto</a></li>
+            </ul>
+          </nav>
+        </div>
     </header>
   )
 }
